18b48890033703c73b6e749666c48673e2fc0a88
svobodovaLucie <xsvobo1x@stud.fit.vutbr.cz>
2021-03-31 11:47:21 +0200

    error handling and free added (todo: needs code refactoring)

--- tail.c
+++ tail.c
@@ -13,6 +13,11 @@
 #define ABS(n) ((n < 0) ? (-n) : (n))
 
 // Function checks if string is a positive number
+// Returns -1 if the number is invalid
+// Returns 1 if the arguments are -n 0 (zero lines to be printed)
+// else returns 0 and the valid num is in variable num
+// var num is positive if the argument is k
+// var num is negative if the argument is +k
 int number_check(char *num_str, long *num) {
     char *ptr;
     long int num_convert = strtol(num_str, &ptr, 10);
@@ -20,13 +25,17 @@ int number_check(char *num_str, long *num) {
         fprintf(stderr, "Invalid argument ( -n %s).\n", num_str);
         return -1;
     }
-    if (num_convert < 0) {
+    if (num_convert < 0 || (num_convert == 0 && num_str[0] == '-')) {
         fprintf(stderr, "Invalid number %ld (must be positive number).\n", num_convert);
         return -1;
     }
 
     if (num_str[0] == '+') {
         num_convert = -(num_convert);
+    }
+
+    if (num_convert == 0 && num_str[0] != '+') {
+        return 1;   // 0 lines to be printed
     }
     *num = num_convert;
     return 0;
@@ -38,7 +47,7 @@ int tail_plus(FILE *file, long int num) {
 
     while (fgets(buffer, MAX_CHARS - 1, file) != NULL) {
         if (buffer[strlen(buffer) - 1] != '\n') {
-            fprintf(stderr, "Moc dlouhe radky.\n");
+            fprintf(stderr, "Max length of a line is %d characters (line %u is longer).\n", MAX_CHARS - 1, line);
             do {
                 fseek(file, 1, SEEK_CUR);
             }
@@ -54,6 +63,13 @@ int tail_plus(FILE *file, long int num) {
     return 0;
 }
 
+void buffer_free(char **buffer, unsigned to_free) {
+    for (unsigned i = 0; i < to_free; i++) {
+        free(buffer[i]);
+    }
+    free(buffer);
+}
+
 int tail(FILE *file, long int num) {
     char **buffer = (char**)calloc(num, sizeof(char*));
     if (buffer == NULL) {
@@ -64,14 +80,14 @@ int tail(FILE *file, long int num) {
         buffer[i] = (char*)calloc(MAX_CHARS, 1);
         if (buffer[i] == NULL) {
             fprintf(file, "Error: Not enough space for memmory allocation (row number %ul)\n", i);
-            //table_free();
+            buffer_free(buffer, i);
         }
     }
 
     unsigned num_of_line = 0;
     while (fgets(buffer[num_of_line % num], MAX_CHARS-1, file) != NULL) {
         if (buffer[num_of_line % num][strlen(buffer[num_of_line % num]) - 1] != '\n') {
-            fprintf(stderr, "Moc dlouhe radky.\n");
+            fprintf(stderr, "Max length of a line is %d characters (line %u is longer).\n", MAX_CHARS - 1, num_of_line);
             int c;
             while ((c = fgetc(file)) != '\n') {
                 ;    // prazdny cyklus
@@ -87,6 +103,8 @@ int tail(FILE *file, long int num) {
         printf("%s", buffer[j % num]);
         j++;
     } while ((j % num) != (num_of_line % num));
+
+    buffer_free(buffer, num);
 
     return 0;
 }
@@ -108,9 +126,20 @@ int main(int argc, char *argv[]) {
         break;
     case 3:
     case 4:
-        if (strcmp(argv[1], "-n") || number_check(argv[2], &num)) {
-            fprintf(stderr, "Error: Invalid arguments.\n");
-            return 1;
+        if (!strcmp(argv[1], "-n")) {
+            int num_check_res = number_check(argv[2], &num);
+            if (num_check_res == -1) {
+                // error fprintf is in the number function
+                //fprintf(stderr, "Error: Invalid arguments yyy.\n");
+                return 1;
+            }
+            if (num_check_res == 1) {
+                // 0 lines to be printed
+                return 0;
+            }
+        } else {
+                fprintf(stderr, "Error: Invalid arguments.\n");
+                return 1;
         }
         if (argc == 4) {
             if((file = fopen(argv[3], "r")) == NULL) {
@@ -125,7 +154,7 @@ int main(int argc, char *argv[]) {
     }
 
     /////////////////////////////
-    if (num < 0) {
+    if (num <= 0) {
         if (tail_plus(file, ABS(num))) {
             return -1;
         }
@@ -136,6 +165,6 @@ int main(int argc, char *argv[]) {
         }
     }
     
-
+    fclose(file);
     return 0;
 }
