e06655b591eefd024826b1252b703a7818a6f190
svobodovaLucie <xsvobo1x@stud.fit.vutbr.cz>
2021-04-04 16:14:26 +0200

    move added (todo fix return values when NULL)

--- htab_move.c
+++ htab_move.c
@@ -1,25 +1,28 @@
 #include "htab.h"
 #include "htab_structs.h"
 
-// t=htab_move(newnumb,t2)
-
-// move konstruktor: vytvoření a inicializace
-// nové tabulky přesunem dat z tabulky t2,
-// t2 nakonec zůstane prázdná a alokovaná
-// (tuto funkci cvičně použijte v programu
-// podmíněným překladem #ifdef MOVETEST)
-
 /**
  * @brief move konstruktor: vytvoření a inicializace nové tabulky přesunem dat z tabulky t2, t2 nakonec zůstane prázdná a alokovaná
  * 
- * @param n 
- * @param from
- * @return 
+ * @param n velikost pole ukazatelu v nove tabulce
+ * @param from tabulka, ze ktere se maji data zkopirovat
+ * @return nove alokovana a naplnena tabulku
  */
 htab_t *htab_move(size_t n, htab_t *from) {
     htab_t *new = htab_init(n);
-
-    // presun dat z from do new (vyuzit lookup_add pravdepodobne)
-
-    //htab_clear(from);
+    for (size_t i = 0; i < from->arr_size; i++) {
+        for (htab_item_t *item = from->arr[i]; item != NULL; item = item->next) {
+            htab_pair_t *pair_new = htab_lookup_add(new, item->pair.key);
+            if (pair_new == NULL) {
+                return NULL;
+            }
+            htab_pair_t *pair_from = htab_find(from, item->pair.key);
+            if (pair_from == NULL) {
+                return NULL;
+            }
+            pair_new->value = pair_from->value;
+        }
+    }
+    htab_clear(from);
+    return new;
 }
