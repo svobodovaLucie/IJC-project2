319efa782a41795757eae358336fb19a9885a8ae
svobodovaLucie <xsvobo1x@stud.fit.vutbr.cz>
2021-04-04 15:12:48 +0200

    free() errors fixed

--- htab_erase.c
+++ htab_erase.c
@@ -1,28 +1,40 @@
-#include <string.h>
 #include <stdlib.h>
-
+#include <string.h>
 #include "htab.h"
 #include "htab_structs.h"
 
-// b=htab_erase(t,key)
 /**
- * @brief zrušení záznamu se zadaným klíčem (úspěch:true)
+ * @brief zrušení záznamu se zadaným klíčem
  * 
  * @param t tabulka, ve ktere se ma zaznam vyhledat
  * @param key klic, podle ktereho se zaznam vyhleda
  * @return true pokud zaznam byl nalezen a odstranen, false pokud nebyl nalezen
  */
-
 bool htab_erase(htab_t * t, htab_key_t key) {
-    for (unsigned i = 0; i < t->arr_size; i++) {
-        for (htab_item_t *item = t->arr[i]; item != NULL; item = item->next) {
-            if (!strcmp(item->pair.key, key)) {
-                htab_item_t *tmp = item;
-                item = item->next;
-                free(item);
-                return true;              
-            }
+    size_t index = (htab_hash_function(key) % t->arr_size);
+    size_t key_len = strlen(key);
+    htab_item_t *item = t->arr[index];
+    htab_item_t *prev = NULL;
+    for (; item != NULL; item = item->next) {
+        size_t item_len = strlen(item->pair.key);
+        if (item_len > key_len) {
+            break;
         }
+        if (key_len == item_len && !strcmp(item->pair.key, key)) {
+            htab_item_t *tmp;
+            if (prev == NULL) {
+                tmp = t->arr[index];
+                t->arr[index] = t->arr[index]->next;
+            } else {
+                tmp = item;
+                prev->next = item->next;
+            }
+            free(tmp->pair.key);
+            free(tmp);
+            t->size--;
+            return true;
+        }
+        prev = item;
     }
     return false;
 }
