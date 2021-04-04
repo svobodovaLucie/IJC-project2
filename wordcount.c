7a94e3f47d943149e031806a7081294768ff0276
svobodovaLucie <xsvobo1x@stud.fit.vutbr.cz>
2021-04-04 11:20:33 +0200

    print function added

--- wordcount.c
+++ wordcount.c
@@ -5,44 +5,30 @@
 #define MAX_CHAR 128
 
 void print_info_about_word (htab_pair_t *data) {
-    printf("%s %u\n", data->key, data->value);
+    printf("%s\t%u\t\tindex: %lu\tstrlen: %lu\n", data->key, data->value, htab_hash_function(data->key)%5, strlen(data->key));
 }
 
-int main(int argc, char *argv[]) {
-    FILE *f;
-    if (argc == 1) {
-        f = stdin;
-    } else if (argc == 2) {
-        f = fopen(argv[1], "r");
-        if(f == NULL) {
-            fprintf(stderr, "Error in opening file");
-            return -1;
-        }
-    } else {
-        fprintf(stderr, "Error: Invalid arguments");
-        return -1;
-    }
+#include <stdlib.h>
+#include <string.h>
+#include "htab_structs.h"
 
+int main(void) {
     htab_t *t = htab_init(5);
-    size_t num = htab_size(t);
-    size_t arr_num = htab_bucket_count(t);
-
-    printf("The hash tab has %ld records and array size of %ld\n", num, arr_num);
     char buffer[MAX_CHAR] = {0};
     
-    while (read_word(buffer, MAX_CHAR, f) != EOF) {
-        htab_lookup_add(t, buffer);
-        htab_find(t, buffer);
+    while (read_word(buffer, MAX_CHAR, stdin) != EOF) {
+        if (htab_lookup_add(t, buffer) == NULL) {
+            goto error_malloc;
+        }
         memset(buffer, '\0', MAX_CHAR);
     }
-   
     htab_for_each(t,print_info_about_word);
 
-    num = htab_size(t);
-    arr_num = htab_bucket_count(t);
-    printf("The hash tab has %ld records and array size of %ld\n", num, arr_num);
-
     htab_free(t);
-    fclose(f);
     return 0;
+
+    error_malloc:
+        fprintf(stderr, "Error: htab_lookup_add: Not enough space for memory allocation.\n");
+        htab_free(t);
+        return 1;
 }
